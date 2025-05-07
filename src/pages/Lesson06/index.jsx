461dec135c3281228fc43f0c6b73260d1c93d178
hase218 <chju23041@g.nihon-u.ac.jp>
2025-05-07 19:25:23 +0900

    L6自分でできた

--- src/pages/Lesson06/index.jsx
+++ src/pages/Lesson06/index.jsx
@@ -4,7 +4,15 @@ import Chart from "../../components/Chart06";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  return input.map( ({gender, y, x}) => {
+    return {
+      color: gender === "男性" ? "blue" : "red",
+      gender,
+      bmi: x/ (y/100)**2,
+      weight: x,
+      height: y,
+    };
+  });
 };
 
 const Lesson = () => {
