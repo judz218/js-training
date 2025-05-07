20a2e711de10a2a7efa434680ec4d3ddd05d1a35
hase218 <chju23041@g.nihon-u.ac.jp>
2025-05-07 19:08:55 +0900

    L5こたえみた

--- src/pages/Lesson05/index.jsx
+++ src/pages/Lesson05/index.jsx
@@ -4,7 +4,25 @@ import Chart from "../../components/Chart05";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const genders = Array.from(new Set(input.map(({ gender }) => gender))); //const genders = ["男性", "女性"];
+  const min = Math.round(Math.min(...input.map(({y}) => y)));
+  const max = Math.round(Math.max(...input.map( ({y}) => y)));
+  const bins 
+    = Array.from({ length: max - min + 1 }) //要素は全てundefined
+        .map((_, i) => { //Array.fromでつくった要素数nの配列に対してmap //引数_は配列の要素を使わない
+          const obj = {
+            bin: (min + i).toString(), //文字列に型変換
+          };
+          for (const gender of genders) { //mapだとgenderを使って新しいものを作ってしまうから、for文でただ参照するだけにしている
+            obj[gender] = 0;
+          }
+          return obj;
+        });
+  for (const { y, gender } of input) {
+    const i = Math.round(y) - min; //身長が配列binsの何番目かを調べる
+    bins[i][gender] += 1;
+  }
+  return bins;
 };
 
 const Lesson = () => {
